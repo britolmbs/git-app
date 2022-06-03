53d13cdd371e2ee0e14c90dc189c74285f988008
britolmbs <britolmbs@hotmail.com>
2022-06-03 16:10:48 -0300

    Ajuste para funcionar

--- src/providers.js
+++ src/providers.js
@@ -1,7 +1,7 @@
 import React from "react";
 import App from "./App";
-import { ResetCSS } from "./global.resetCSS";
-import GithubProvider from "./providers/github-providers";
+import { ResetCSS } from "./global/resetCSS";
+import GithubProvider from "./providers/github-provider";
 
 const Providers =() => {
     return (
